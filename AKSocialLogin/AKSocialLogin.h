055bfb362438a2b2b339a3e9a79bde04838ea04e
aadil <aadil@zaptechsolutions.com>
2015-10-27 18:25:02 +0530

    Initial Commit

---
+++ AKSocialLogin/AKSocialLogin.h
@@ -0,0 +1,19 @@
+//
+//  AKSocialLogin.h
+//  AKSocialLogin
+//
+//  Created by aadil on 27/10/15.
+//  Copyright © 2015 zaptechsolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AKSocialLogin.
+FOUNDATION_EXPORT double AKSocialLoginVersionNumber;
+
+//! Project version string for AKSocialLogin.
+FOUNDATION_EXPORT const unsigned char AKSocialLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKSocialLogin/PublicHeader.h>
+
+
